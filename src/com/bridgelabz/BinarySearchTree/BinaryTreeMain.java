8558760ed79a7c3b5d8a41ac59a45b8019e11bf3
amolmane7471 <maneamol3233@gmail.com>
2022-09-08 16:06:30 +0530

    [Amol] : added Binary search tree of 56,30 and 70

--- src/com/bridgelabz/BinarySearchTree/BinaryTreeMain.java
+++ src/com/bridgelabz/BinarySearchTree/BinaryTreeMain.java
@@ -1,7 +1,18 @@
 package com.bridgelabz.BinarySearchTree;
-
+/*
+ * purpose : Ability to create a BST by adding 56 and then adding 30 & 70 , Use INode to create My Binary
+ * @uthor : Amol
+ * since : 2022/09/08
+ */
 public class BinaryTreeMain {
 	public static void main(String[] args) {
 		System.out.println("------ Welcome to Data Structure : Binary Search Tree ------");	
+		
+		BinaryTree<Integer> bTree = new BinaryTree<Integer>();
+		bTree.add(56);
+		bTree.add(30);
+		bTree.add(70);
+		
+		bTree.printBinaryTree();
 		}
 }
